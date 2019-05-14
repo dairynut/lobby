656d9f17a2d7c907943deeb7b0008e3f3da87682
Craig Ashley <craig.g.ashley@gmail.com>
2019-05-14 22:11:17 +0100

    Initial checkin of lobby application example. Modified version of the base .NET Core MVC project that utilises SignalR to initialise a chat window on the index page. Included simple methods for sending and receiving messages globally to all connected clients. Also included a currently unused method for sending private messages to a specified client. Added some barebones HTML, jQuery and CSS for some very rough styling of an ideal chat window format.

---
+++ Lobby/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Lobby.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAllUsers(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+
+        public async Task SendMessageToSpecifiedUser(string senderUsername, string recipientConnectionId, string message)
+        {
+            await Clients.Client(recipientConnectionId).SendAsync("ReceiveMessage", senderUsername, message);
+        }
+    }
+}
---
+++ Lobby/wwwroot/js/lobby.chat-0.1.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("confirmName").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg =  user.bold() + ": " + msg;
+    var li = document.createElement("li");
+    li.innerHTML = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+    $('.messageDisplayList').scrollTop($('.messageDisplayList')[0].scrollHeight);
+});
+
+connection.start().then(function () {
+    document.getElementById("confirmName").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("confirmName").addEventListener("click", function (event) {
+    $('#userField').addClass('hidden');
+    $('#messageInputField').removeClass('hidden');
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessageToAllUsers", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
